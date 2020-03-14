a4e19e9b811ec2ffedcdc4266b1e6fd85067516a
Megan Christine McClure <mcclme02@luther.edu>
2020-03-14 19:49:53 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  #/^un...ing$/
+  #/un\w*ing/
+  text.match(/un\w*ing/) 
 end
 
 def words_five_letters_long(text)
